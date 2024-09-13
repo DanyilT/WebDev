e214c49f462dad4bbb522c148e5eba9b8a6400b8
DanyilT <drtymchuk@gmail.com>
2024-09-13 02:27:00 +0100

    Load Assets Script

--- PersonalPortfolio/scripts/load-assets.js
+++ PersonalPortfolio/scripts/load-assets.js
@@ -18,6 +18,26 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
             document.getElementById('header').insertAdjacentHTML('beforebegin', header.outerHTML);
             document.getElementById('header').remove();
+        })
+        // Catch Header from the GitHub Pages
+        .catch(() => {
+            fetch('https://danyilt.github.io/WebDev/PersonalPortfolio/pages/assets/header.html')
+                .then(response => response.text())
+                .then(data => {
+                    const header = new DOMParser().parseFromString(data, 'text/html').querySelector('header');
+                    header.querySelectorAll('a').forEach((link, index) => {
+                        const href = link.getAttribute('href');
+                        if (href && index !== 0) {
+                            link.setAttribute('href', (isHomePage ? 'pages/' : '../') + href);
+                        }
+                    });
+                    if (!isHomePage) {
+                        header.querySelector('a:first-child').setAttribute('href', '../index.html');
+                    }
+
+                    document.getElementById('header').insertAdjacentHTML('beforebegin', header.outerHTML);
+                    document.getElementById('header').remove();
+                });
         });
 
     // Load the footer
@@ -32,6 +52,21 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
             document.getElementById('footer').insertAdjacentHTML('beforebegin', footer.outerHTML);
             document.getElementById('footer').remove();
+        })
+        // Catch Footer from the GitHub Pages
+        .catch(() => {
+            fetch('https://danyilt.github.io/WebDev/PersonalPortfolio/pages/assets/footer.html')
+                .then(response => response.text())
+                .then(data => {
+                    const footer = new DOMParser().parseFromString(data, 'text/html').querySelector('footer');
+                    const sitemapLink = footer.querySelector('a[href*="sitemap.html"]');
+                    if (isHomePage) {
+                        sitemapLink.setAttribute('href', 'pages/' + sitemapLink.getAttribute('href'));
+                    }
+
+                    document.getElementById('footer').insertAdjacentHTML('beforebegin', footer.outerHTML);
+                    document.getElementById('footer').remove();
+                });
         });
 
     // Load the sidebar
@@ -43,5 +78,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
             document.getElementById('sidebar').remove();
             document.getElementById('links').remove();
+        })
+        // Catch Sidebar from the GitHub Pages
+        .catch(() => {
+            fetch('https://danyilt.github.io/WebDev/PersonalPortfolio/pages/assets/sidebar.html')
+                .then(response => response.text())
+                .then(data => {
+                    document.getElementById('sidebar').insertAdjacentHTML('beforebegin', data);
+                    document.getElementById('links').insertAdjacentHTML('beforebegin', data);
+
+                    document.getElementById('sidebar').remove();
+                    document.getElementById('links').remove();
+                });
         });
 });
