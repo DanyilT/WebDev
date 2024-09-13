5896e4e46eeb28dd7e387adddd777519d3398d86
DanyilT <drtymchuk@gmail.com>
2024-09-13 01:46:36 +0100

    Load Asset Script

---
+++ PersonalPortfolio/scripts/load-assets.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Load the header
+    fetch('pages/assets/header.html')
+        .then(response => response.text())
+        .then(data => {
+            const header = new DOMParser().parseFromString(data, 'text/html').querySelector('header');
+            if (window.location.pathname.endsWith('index.html')) {
+                header.querySelectorAll('a').forEach((link, index) => {
+                    const href = link.getAttribute('href');
+                    if (href && index !== 0) {
+                        link.setAttribute('href', 'pages/' + href);
+                    }
+                });
+            } else {
+                header.querySelector('a:first-child').setAttribute('href', '../index.html');
+            }
+
+            document.getElementById('header').insertAdjacentHTML('beforebegin', header.outerHTML);
+            document.getElementById('header').remove();
+        });
+
+    // Load the footer
+    fetch('pages/assets/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            const footer = new DOMParser().parseFromString(data, 'text/html').querySelector('footer');
+            const sitemapLink = footer.querySelector('a[href*="sitemap.html"]');
+            if (window.location.pathname.endsWith('index.html')) {
+                sitemapLink.setAttribute('href', 'pages/' + sitemapLink.getAttribute('href'));
+            }
+
+            document.getElementById('footer').insertAdjacentHTML('beforebegin', footer.outerHTML);
+            document.getElementById('footer').remove();
+        });
+
+    // Load the sidebar
+    fetch('pages/assets/sidebar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('sidebar').insertAdjacentHTML('beforebegin', data);
+            document.getElementById('links').insertAdjacentHTML('beforebegin', data);
+
+            document.getElementById('sidebar').remove();
+            document.getElementById('links').remove();
+        });
+});
